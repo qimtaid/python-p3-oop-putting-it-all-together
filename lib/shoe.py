385a230594a1c6a412e84d40b4f021e917f74b77
Dan Koskei <qimtaid@gmail.com>
2024-05-26 21:47:51 +0300

    complete lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = self._validate_size(size)
+        self.condition = "Used"
+
+    def _validate_size(self, size):
+        if not isinstance(size, int):
+            print("size must be an integer")
+            return 0
+        return size
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
